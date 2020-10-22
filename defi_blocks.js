9756b7df2ca426ce9f710532fb231421667b74ab
Marius Podwyszynski <marius.pod@gmail.com>
2020-10-22 21:53:05 +0200

    Cleanup

--- defi_blocks.js
+++ defi_blocks.js
@@ -1,43 +1,6 @@
 var Blockly = require('blockly');
 
-Blockly.defineBlocksWithJsonArray([{
-  "type": "object",
-  "message0": "{ %1 %2 }",
-  "args0": [
-    {
-      "type": "input_dummy"
-    },
-    {
-      "type": "input_statement",
-      "name": "MEMBERS"
-    }
-  ],
-  "output": null,
-  "colour": 130,
-},
-{
-  "type": "member",
-  "message0": "%1 %2 %3",
-  "args0": [
-    {
-      "type": "field_input",
-      "name": "MEMBER_NAME",
-      "text": ""
-    },
-    {
-      "type": "field_label",
-      "name": "COLON",
-      "text": ":"
-    },
-    {
-      "type": "input_value",
-      "name": "MEMBER_VALUE"
-    }
-  ],
-  "previousStatement": null,
-  "nextStatement": null,
-  "colour": 230,
-},
+Blockly.defineBlocksWithJsonArray([
 {
   "type": "strategy",
   "message0": "Strategy name %1 Tokens %2 Performance fee %3 Performance max %4 Withdrawal fee %5 Withdrawal max %6 Depositor %7 Rewardor %8 Exchange %9",
